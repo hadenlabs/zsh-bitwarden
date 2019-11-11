0d0bfcdcf293aae64f084528fcf93b3a9eb76874
Luis Mayta <luis@hadenlabs.com>
2019-11-10 19:12:11 -0500

    chore: implement actions for make (#1)

---
+++ provision/make/docs.mk
@@ -0,0 +1,21 @@
+#
+# See ./CONTRIBUTING.rst
+#
+
+FILE_README=$(ROOT_DIR)/README.rst
+
+docs:
+	make docs.help
+
+docs.help:
+	@echo '    Docs:'
+	@echo ''
+	@echo '        docs.show                  Show restview README'
+	@echo '        docs.make                  Make documentation html'
+	@echo ''
+
+docs.show: clean
+	$(PIPENV_RUN) restview ${FILE_README}
+
+docs.make: clean
+	$(docker-compose) -f ${PATH_DOCKER_COMPOSE}/dev.yml run --rm docs bash -c "cd docs && make html"

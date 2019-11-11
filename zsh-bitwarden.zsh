1589def27b5c49d57473e083420c7d5672084dc0
@slovacus <slovacus@gmail.com>
2019-11-11 00:11:05 -0500

    (#3) chore: implement functions for list bitwarden

--- zsh-bitwarden.zsh
+++ zsh-bitwarden.zsh
@@ -8,13 +8,59 @@
 #   Luis Mayta <slovacus@gmail.com>
 #
 
-plugin_dir="$(dirname ${0}:A)"
+plugin_dir=$(dirname "${0}":A)
+
+# shellcheck source=/dev/null
 source "${plugin_dir}"/src/helpers/messages.zsh
 
 PACKAGE_NAME='@bitwarden/cli'
 
 die(){
     message_error "$1";
+}
+
+_get_type () {
+    local bw_type
+    bw_type="${1}"
+    echo "${bw_type}" \
+        | awk '{
+            split($0, array, "|")
+            print array[1]
+        }'
+}
+
+_get_id () {
+    local bw_id
+    bw_id=${1}
+    echo "$bw_id" \
+        | awk '{
+            split($0, array, "|")
+            print array[2]
+        }'
+}
+
+_get_type_field () {
+    local bw_type
+    bw_type=$(_get_type "${1}")
+    if [ "${bw_type}" -eq "1" ]
+    then
+        echo ".login.password"
+    elif [ "${bw_type}" -eq "2" ]
+    then
+        echo ".notes";
+    fi
+}
+
+_get_item_by_type() {
+    local bw_id
+    local bw_type_field
+    bw_type_field=$(_get_type_field "${1}")
+    bw_id=$(_get_id "${1}")
+    bw get item "${bw_id}"  \
+        | jq "${bw_type_field}" \
+        | sed 's/\"//g' \
+        | perl -pe 'chomp' \
+        | pbcopy
 }
 
 function bw::validation {
@@ -36,22 +82,20 @@ function bw::install {
     message_info "Installing ${PACKAGE_NAME}"
     bw::validation
     yarn global add ${PACKAGE_NAME}
-    bw::post_install
 }
 
 function bw::search {
     if hash bw 2>/dev/null; then
-        bw get item \
-           "$(bw list items \
-              | jq '.[] | "\(.name) | username: \(.login.username) | id: \(.id)" ' \
-              | fzf \
-              | awk '{print $(NF -0)}' \
-              | sed 's/\"//g'
-            )" \
-                | jq '.login.password' \
-                | sed 's/\"//g' \
-                | perl -pe 'chomp' \
-                | pbcopy
+        local bw_type_id
+        bw_type_id=$(bw list items \
+                         | jq '.[] | "\(.type) | \(.name) | username: \(.login.username) | id: \(.type)|\(.id)" ' \
+                         | fzf \
+                         | awk '{print $(NF -0)}' \
+                         | perl -pe 'chomp' \
+                         | sed 's/\"//g'
+                  )
+        _get_item_by_type "${bw_type_id}"
+
     fi
 }
 

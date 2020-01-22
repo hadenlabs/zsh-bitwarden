3b209107df50a488c6bdf99d2ad46873e1049d22
Luis Mayta <luis@hadenlabs.com>
2020-01-22 05:01:22 -0500

    refactor: function for bitwarden separated (#11)

--- zsh-bitwarden.zsh
+++ zsh-bitwarden.zsh
@@ -15,49 +15,8 @@ BW_SRC_DIR="${BW_ROOT_DIR}"/src
 # shellcheck source=/dev/null
 source "${BW_SRC_DIR}"/base.zsh
 
-_get_type () {
-    local bw_type
-    bw_type="${1}"
-    echo "${bw_type}" \
-        | awk '{
-            split($0, array, "|")
-            print array[1]
-        }'
-}
-
-_get_id () {
-    local bw_id
-    bw_id=${1}
-    echo "$bw_id" \
-        | awk '{
-            split($0, array, "|")
-            print array[2]
-        }'
-}
-
-_get_type_field () {
-    local bw_type
-    bw_type=$(_get_type "${1}")
-    if [ "${bw_type}" -eq "1" ]
-    then
-        echo ".login.password"
-    elif [ "${bw_type}" -eq "2" ]
-    then
-        echo ".notes";
-    fi
-}
-
-_get_item_by_type() {
-    local bw_id
-    local bw_type_field
-    bw_type_field=$(_get_type_field "${1}")
-    bw_id=$(_get_id "${1}")
-    bw get item "${bw_id}"  \
-        | jq "${bw_type_field}" \
-        | sed 's/\"//g' \
-        | perl -pe 'chomp' \
-        | pbcopy
-}
+# shellcheck source=/dev/null
+source "${BW_SRC_DIR}"/bitwarden.zsh
 
 function bw::install {
     local has_dependences
@@ -69,21 +28,4 @@ function bw::install {
     fi
 }
 
-function bw::search {
-    if hash bw 2>/dev/null; then
-        local bw_type_id
-        bw_type_id=$(bw list items \
-                         | jq '.[] | "\(.type) | \(.name) | username: \(.login.username) | id: \(.type)|\(.id)" ' \
-                         | fzf \
-                         | awk '{print $(NF -0)}' \
-                         | perl -pe 'chomp' \
-                         | sed 's/\"//g'
-                  )
-        _get_item_by_type "${bw_type_id}"
-
-    fi
-}
-
-if ! type -p bw > /dev/null; then
-    bw::install
-fi
+if ! type -p bw > /dev/null; then bw::install; fi

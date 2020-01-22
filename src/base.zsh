e40ca8b4219b0cb34d475d83bc47b9f5d9ef287b
Luis Mayta <luis@hadenlabs.com>
2020-01-22 05:46:37 -0500

    chore: Implement validation for bw session (#11)

--- src/base.zsh
+++ src/base.zsh
@@ -1,6 +1,14 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
+function bw::has_bw_session {
+    if [ -z "${BW_SESSION}" ]; then
+        echo 0
+        return
+    fi
+    echo 1
+}
+
 function bw::node::install {
     if ! type nvm > /dev/null; then
         message_warning "is Neccesary install nvm, please install antibody bundle luismayta/zsh-nvm branch:develop"
@@ -44,5 +52,8 @@ function bw::has_dependences {
 }
 
 
+if [ "$(bw::has_bw_session)" -eq 0 ]; then
+    message_warning "PLease add the value of BW_SESSION"
+fi
 if [ "$(bw::has_node)" -eq 0 ]; then bw::node::install; fi
 if [ "$(bw::has_yarn)" -eq 0 ]; then bw::yarn::install; fi
